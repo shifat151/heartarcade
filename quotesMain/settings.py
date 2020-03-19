2cc4155131a63fc1f3dfbead14f3b348ee31f142
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-03-19 12:13:57 +0600

    setting file modified

--- quotesMain/settings.py
+++ quotesMain/settings.py
@@ -164,7 +164,7 @@ DATABASES = {
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),
-        'PORT':config('PORT', cast=int)
+        'PORT':5432
     }
 }
 

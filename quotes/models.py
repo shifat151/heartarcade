8425c80ce38b7eb5391cd464f795fb59695ba98b
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-03-19 11:36:02 +0600

    2nd version ready for deployment on heroku

--- quotes/models.py
+++ quotes/models.py
@@ -14,7 +14,7 @@ class Quote(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     quote=models.TextField()
     author=models.ForeignKey(User, on_delete=models.CASCADE)
-    categories=models.ManyToManyField(QuoteCategory, blank=True)
+    categories=models.ManyToManyField(QuoteCategory, blank=False)
     pub_date=models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- quotesMain/settings.py
+++ quotesMain/settings.py
@@ -1,5 +1,7 @@
+import django_heroku
 import os
 from decouple import config, Csv
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -13,9 +15,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -162,7 +164,7 @@ DATABASES = {
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),
-        'PORT':'5432',
+        'PORT':config('PORT', cast=int)
     }
 }
 
@@ -204,10 +206,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'quotesMain/static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())

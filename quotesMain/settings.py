393a59bee90f53b28180bf2a2a0ff9862fd04651
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-09-05 20:58:02 +0600

    Add quote button changed

--- quotesMain/settings.py
+++ quotesMain/settings.py
@@ -209,9 +209,9 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'quotesMain/static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())

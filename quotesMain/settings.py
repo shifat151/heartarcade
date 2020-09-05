24046988376b7f421b32ec705b04dab1d29943f7
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-09-05 21:40:17 +0600

    static file folder changed

--- quotesMain/settings.py
+++ quotesMain/settings.py
@@ -205,13 +205,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+# python manage.py collects the static files into static roots
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'quotesMain/static')
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
